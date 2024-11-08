91f3d3a62226fb887c8b0626981577f5aa5eb133
AlexCavalcantiJordao <alex37705732@gmail.com>
2024-11-08 17:50:48 -0300

    db_MeusLivros

---
+++ db_MeusLivros.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    con = mysql.connector.connect(
+        host='localhost',
+        database='db_MeusLivros',
+        user='root',
+        password='1234567'
+    )
+
+    if con.is_connected():
+        db_info = con.get_server_info()
+        print("Conectado ao servidor MySQL versão ", db_info)
+        cursor = con.cursor()
+        cursor.execute("SELECT DATABASE();")
+        linha = cursor.fetchone()
+        print("Conectado ao banco de dados", linha)
+
+except Error as e:
+    print("Erro ao conectar ao MySQL", e)
+
+finally:
+    if con.is_connected():
+        cursor.close()
+        con.close()
+        print("Conexão ao MySQL foi encerrada.")
